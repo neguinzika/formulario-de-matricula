a7169fe0093c1a4ad09166c048c513a79b53718d
Luiz <luizcruz02059345@gmail.com>
2025-04-15 21:34:05 -0300

    layout CSS

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,21 @@
 <template>
-  .
+
+  <main>
+    <div class="main-container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, adipisci asperiores consectetur corporis doloremque
+      eius eligendi enim impedit in libero nemo odit perferendis perspiciatis quasi reiciendis tenetur totam velit
+      vitae.
+
+    </div>
+  </main>
+  <aside>
+    <div class="aside-container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, explicabo voluptate! Ab adipisci alias at,
+      corporis dicta dignissimos dolore doloribus eligendi id iure laboriosam numquam quasi recusandae sapiente sed
+      unde.
+
+    </div>
+  </aside>
 </template>
 
 <script>
@@ -14,3 +30,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style src="./global.css"/>
+<style src="./layout.css"/>
